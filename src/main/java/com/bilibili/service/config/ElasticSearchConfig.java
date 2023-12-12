dfa0c4e27a86e0c6b68c0802c29f035f8d20ce5d
davidlee <david.wushi@foxmail.com>
2023-12-12 19:45:15 +0800

    修改es配置

--- src/main/java/com/bilibili/service/config/ElasticSearchConfig.java
+++ src/main/java/com/bilibili/service/config/ElasticSearchConfig.java
@@ -3,6 +3,7 @@ package com.bilibili.service.config;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
@@ -10,18 +11,14 @@ import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfig
 @Configuration
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
-//    @Value("${spring.elasticsearch.uris}")
-//    private String esUrl;
+    @Value("${spring.elasticsearch.url}")
+    private String esUrl;
 
     @Override
     @Bean
     public RestHighLevelClient elasticsearchClient(){
-//        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-//                .connectedTo(esUrl)
-//                .build();
-//        return RestClients.create(clientConfiguration).rest();
         RestHighLevelClient restHighLevelClient = new RestHighLevelClient(RestClient.builder(
-                new HttpHost("49.235.137.33", 9200, "http")));
+                new HttpHost(esUrl, 9200, "http")));
         return restHighLevelClient;
     }
 }
