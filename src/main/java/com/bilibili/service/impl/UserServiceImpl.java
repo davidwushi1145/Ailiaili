0fcee9eeb6cda52ee8419cd62430560c9ab64b21
davidlee <david.wushi@foxmail.com>
2023-12-19 22:33:12 +0800

    完善报表系统

--- src/main/java/com/bilibili/service/impl/UserServiceImpl.java
+++ src/main/java/com/bilibili/service/impl/UserServiceImpl.java
@@ -93,7 +93,6 @@ public class UserServiceImpl
       throw new ConditionException("当前用户不存在");
     }
     String userPassword = user.getUserPassword();
-    userPassword = RSAUtil.encrypt(userPassword);
     String rawPassword;
     try {
       rawPassword = RSAUtil.decrypt(userPassword);
