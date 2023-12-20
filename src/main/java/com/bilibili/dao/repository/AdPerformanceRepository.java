40ee99f6065e2851f95227bf9ca84a29119c863c
Wgz <1808365012@mail.ynu.edu.cn>
2023-12-21 01:30:59 +0800

    添加前端展示用户主页需要的接口

--- src/main/java/com/bilibili/dao/repository/AdPerformanceRepository.java
+++ src/main/java/com/bilibili/dao/repository/AdPerformanceRepository.java
@@ -2,7 +2,9 @@ package com.bilibili.dao.repository;
 
 import com.bilibili.dao.domain.TAdPerformance;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdPerformanceRepository
     extends ElasticsearchRepository<TAdPerformance, Long> {
   TAdPerformance findByAdId(Long adId);
