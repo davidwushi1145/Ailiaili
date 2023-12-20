b480663732e468ddd654b15972fb22ba3794fdc5
Restyled.io <commits@restyled.io>
2023-12-20 03:41:00 +0000

    Restyled by clang-format

--- src/main/java/com/bilibili/api/VideoApi.java
+++ src/main/java/com/bilibili/api/VideoApi.java
@@ -3,14 +3,12 @@ package com.bilibili.api;
 import com.bilibili.api.support.UserSupport;
 import com.bilibili.dao.domain.*;
 import com.bilibili.service.*;
-
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.swing.text.html.ListView;
-
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,206 +16,199 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class VideoApi {
 
-    @Autowired
-    private VideoService videoService;
+  @Autowired private VideoService videoService;
 
-    @Autowired
-    private UserSupport userSupport;
+  @Autowired private UserSupport userSupport;
 
-    @Autowired
-    private VideoLikeService videoLikeService;
+  @Autowired private VideoLikeService videoLikeService;
 
-    @Autowired
-    private VideoCollectionService videoCollectionService;
+  @Autowired private VideoCollectionService videoCollectionService;
 
-    @Autowired
-    private VideoCoinService videoCoinService;
+  @Autowired private VideoCoinService videoCoinService;
 
-    @Autowired
-    private VideoCommentService videoCommentService;
+  @Autowired private VideoCommentService videoCommentService;
 
-    @Autowired
-    private VideoViewService videoViewService;
+  @Autowired private VideoViewService videoViewService;
 
-    @Autowired
-    private ElasticSearchService elasticSearchService;
+  @Autowired private ElasticSearchService elasticSearchService;
 
+  // 添加视频
+  @PostMapping("/videos")
+  public JsonResponse<String> addVideos(@RequestBody Video video) {
+    Long userId = userSupport.getCurrentUserId();
+    video.setUserId(userId);
+    video.setCreateTime(new Date());
+    video.setCoins(0);
+    video.setCollections(0);
+    video.setLikes(0);
+    video.setViews(0);
+    videoService.addVideos(video);
+    // 添加到es
+    elasticSearchService.addVideo(videoService.getById(video.getId()));
+    return JsonResponse.success();
+  }
 
+  // 分页查询
+  @GetMapping("/get-videos")
+  public JsonResponse<PageResult<Video>>
+  pageListVideos(Integer size, Integer page, String area) {
+    PageResult<Video> result = videoService.pageListVideos(size, page, area);
+    return new JsonResponse<>(result);
+  }
 
-    // 添加视频
-    @PostMapping("/videos")
-    public JsonResponse<String> addVideos(@RequestBody Video video) {
-        Long userId = userSupport.getCurrentUserId();
-        video.setUserId(userId);
-        video.setCreateTime(new Date());
-        video.setCoins(0);
-        video.setCollections(0);
-        video.setLikes(0);
-        video.setViews(0);
-        videoService.addVideos(video);
-        // 添加到es
-        elasticSearchService.addVideo(videoService.getById(video.getId()));
-        return JsonResponse.success();
+  // 视频在线观看
+  @GetMapping("/video-slices")
+  public void viewVideoOnlineBySlices(HttpServletRequest request,
+                                      HttpServletResponse response, String url)
+      throws Exception {
+    videoService.viewVideoOnlineBySlices(request, response, url);
+  }
+
+  // 视频点赞
+  @PostMapping("/video-likes")
+  public JsonResponse<String> addVideoLike(@RequestParam Long videoId) {
+    Long userId = userSupport.getCurrentUserId();
+    videoLikeService.addVideoLike(videoId, userId);
+    elasticSearchService.updateVideo(videoService.getById(videoId));
+    return JsonResponse.success();
+  }
+
+  // 取消点赞
+  @DeleteMapping("/video-likes")
+  public JsonResponse<String> deleteVideoLikes(@RequestParam Long videoId) {
+    Long userId = userSupport.getCurrentUserId();
+    videoLikeService.deleteVideoLike(videoId, userId);
+    elasticSearchService.updateVideo(videoService.getById(videoId));
+    return JsonResponse.success();
+  }
+
+  // 查询视频点赞数量
+  @GetMapping("video-likes")
+  public JsonResponse<Map<String, Object>>
+  getVideoLikes(@RequestParam Long videoId) {
+    Long userId = null;
+    try {
+      userId = userSupport.getCurrentUserId();
+    } catch (Exception e) {
     }
+    Map<String, Object> map = videoLikeService.getVideoLike(videoId, userId);
+    return new JsonResponse<>(map);
+  }
 
-    // 分页查询
-    @GetMapping("/get-videos")
-    public JsonResponse<PageResult<Video>>
-    pageListVideos(Integer size, Integer page, String area) {
-        PageResult<Video> result = videoService.pageListVideos(size, page, area);
-        return new JsonResponse<>(result);
-    }
+  // 收藏视频
+  @PostMapping("/video-collections")
+  public JsonResponse<String>
+  addVideoCollection(@RequestBody VideoCollection videoCollection) {
+    Long userId = userSupport.getCurrentUserId();
+    videoCollectionService.addVideoCollection(videoCollection, userId);
+    elasticSearchService.updateVideo(
+        videoService.getById(videoCollection.getVideoId()));
+    return JsonResponse.success();
+  }
 
-    // 视频在线观看
-    @GetMapping("/video-slices")
-    public void viewVideoOnlineBySlices(HttpServletRequest request,
-                                        HttpServletResponse response, String url)
-            throws Exception {
-        videoService.viewVideoOnlineBySlices(request, response, url);
-    }
+  // 取消收藏视频
+  @DeleteMapping("/video-collections")
+  public JsonResponse<String>
+  deleteVideoCollection(@RequestParam Long videoId) {
+    Long userId = userSupport.getCurrentUserId();
+    videoCollectionService.deleteVideoCollections(videoId, userId);
+    elasticSearchService.updateVideo(videoService.getById(videoId));
+    return JsonResponse.success();
+  }
 
-    // 视频点赞
-    @PostMapping("/video-likes")
-    public JsonResponse<String> addVideoLike(@RequestParam Long videoId) {
-        Long userId = userSupport.getCurrentUserId();
-        videoLikeService.addVideoLike(videoId, userId);
-        elasticSearchService.updateVideo(videoService.getById(videoId));
-        return JsonResponse.success();
+  // 获取当前视频收藏量
+  @GetMapping("/video-collections")
+  public JsonResponse<Map<String, Object>>
+  getVideoCollections(@RequestParam Long videoId) {
+    Long userId = null;
+    try {
+      userId = userSupport.getCurrentUserId();
+    } catch (Exception e) {
     }
+    Map<String, Object> map =
+        videoCollectionService.getVideoCollections(videoId, userId);
+    return new JsonResponse<>(map);
+  }
 
-    // 取消点赞
-    @DeleteMapping("/video-likes")
-    public JsonResponse<String> deleteVideoLikes(@RequestParam Long videoId) {
-        Long userId = userSupport.getCurrentUserId();
-        videoLikeService.deleteVideoLike(videoId, userId);
-        elasticSearchService.updateVideo(videoService.getById(videoId));
-        return JsonResponse.success();
-    }
+  // 视频投币
+  @PostMapping("/video-coins")
+  public JsonResponse<String> addVideoCoins(@RequestBody VideoCoin videoCoin) {
+    Long userId = userSupport.getCurrentUserId();
+    videoCoinService.addVideoCions(videoCoin, userId);
+    elasticSearchService.updateVideo(
+        videoService.getById(videoCoin.getVideoId()));
+    return JsonResponse.success();
+  }
 
-    // 查询视频点赞数量
-    @GetMapping("video-likes")
-    public JsonResponse<Map<String, Object>>
-    getVideoLikes(@RequestParam Long videoId) {
-        Long userId = null;
-        try {
-            userId = userSupport.getCurrentUserId();
-        } catch (Exception e) {
-        }
-        Map<String, Object> map = videoLikeService.getVideoLike(videoId, userId);
-        return new JsonResponse<>(map);
+  // 查询视频投币数量
+  @GetMapping("/video-coins")
+  public JsonResponse<Map<String, Object>>
+  getVideoCoins(@RequestParam Long videoId) {
+    Long userId = null;
+    try {
+      userId = userSupport.getCurrentUserId();
+    } catch (Exception e) {
     }
+    Map<String, Object> map = videoCoinService.getVideoCoins(videoId, userId);
+    return new JsonResponse<>(map);
+  }
 
-    // 收藏视频
-    @PostMapping("/video-collections")
-    public JsonResponse<String>
-    addVideoCollection(@RequestBody VideoCollection videoCollection) {
-        Long userId = userSupport.getCurrentUserId();
-        videoCollectionService.addVideoCollection(videoCollection, userId);
-        elasticSearchService.updateVideo(videoService.getById(videoCollection.getVideoId()));
-        return JsonResponse.success();
-    }
+  // 添加视频评论
+  @PostMapping("/video-comments")
+  public JsonResponse<String>
+  addVideoComments(@RequestBody VideoComment videoComment) {
+    Long userId = userSupport.getCurrentUserId();
+    videoCommentService.addVideoComments(videoComment, userId);
+    return JsonResponse.success();
+  }
 
-    // 取消收藏视频
-    @DeleteMapping("/video-collections")
-    public JsonResponse<String>
-    deleteVideoCollection(@RequestParam Long videoId) {
-        Long userId = userSupport.getCurrentUserId();
-        videoCollectionService.deleteVideoCollections(videoId, userId);
-        elasticSearchService.updateVideo(videoService.getById(videoId));
-        return JsonResponse.success();
-    }
+  // 分页查询视频评论
+  @GetMapping("/video-comments")
+  public JsonResponse<PageResult<VideoComment>>
+  pageListVideoComments(@RequestParam Long size, @RequestParam Integer number,
+                        @RequestParam Long videoId) {
+    PageResult<VideoComment> pageResult =
+        videoCommentService.pageListVideoComments(size, number, videoId);
+    return new JsonResponse<>(pageResult);
+  }
 
-    // 获取当前视频收藏量
-    @GetMapping("/video-collections")
-    public JsonResponse<Map<String, Object>>
-    getVideoCollections(@RequestParam Long videoId) {
-        Long userId = null;
-        try {
-            userId = userSupport.getCurrentUserId();
-        } catch (Exception e) {
-        }
-        Map<String, Object> map =
-                videoCollectionService.getVideoCollections(videoId, userId);
-        return new JsonResponse<>(map);
-    }
+  // 获取视频详情
+  @GetMapping("/video-details")
+  public JsonResponse<Map<String, Object>>
+  getVideoDetails(@RequestParam Long videoId) {
+    Map<String, Object> result = videoService.getVideoDetails(videoId);
+    return new JsonResponse<>(result);
+  }
 
-    // 视频投币
-    @PostMapping("/video-coins")
-    public JsonResponse<String> addVideoCoins(@RequestBody VideoCoin videoCoin) {
-        Long userId = userSupport.getCurrentUserId();
-        videoCoinService.addVideoCions(videoCoin, userId);
-        elasticSearchService.updateVideo(videoService.getById(videoCoin.getVideoId()));
-        return JsonResponse.success();
+  // 添加视频观看记录
+  @PostMapping("/video-views")
+  public JsonResponse<String> addVideoView(@RequestBody VideoView videoView,
+                                           HttpServletRequest request) {
+    Long userId;
+    try {
+      userId = userSupport.getCurrentUserId();
+      videoView.setUserId(userId);
+      videoViewService.addVideoView(videoView, request);
+      elasticSearchService.updateVideo(
+          videoService.getById(videoView.getVideoId()));
+    } catch (Exception e) {
+      videoViewService.addVideoView(videoView, request);
     }
+    return JsonResponse.success();
+  }
 
-    // 查询视频投币数量
-    @GetMapping("/video-coins")
-    public JsonResponse<Map<String, Object>>
-    getVideoCoins(@RequestParam Long videoId) {
-        Long userId = null;
-        try {
-            userId = userSupport.getCurrentUserId();
-        } catch (Exception e) {
-        }
-        Map<String, Object> map = videoCoinService.getVideoCoins(videoId, userId);
-        return new JsonResponse<>(map);
-    }
+  // 统计观看人数
+  @GetMapping("/video-view-counts")
+  public JsonResponse<Integer> getVideoViewCounts(@RequestParam Long videoId) {
+    Integer count = videoViewService.getVideoViewCounts(videoId);
+    return new JsonResponse<>(count);
+  }
 
-    // 添加视频评论
-    @PostMapping("/video-comments")
-    public JsonResponse<String>
-    addVideoComments(@RequestBody VideoComment videoComment) {
-        Long userId = userSupport.getCurrentUserId();
-        videoCommentService.addVideoComments(videoComment, userId);
-        return JsonResponse.success();
-    }
-
-    // 分页查询视频评论
-    @GetMapping("/video-comments")
-    public JsonResponse<PageResult<VideoComment>>
-    pageListVideoComments(@RequestParam Long size, @RequestParam Integer number,
-                          @RequestParam Long videoId) {
-        PageResult<VideoComment> pageResult =
-                videoCommentService.pageListVideoComments(size, number, videoId);
-        return new JsonResponse<>(pageResult);
-    }
-
-    // 获取视频详情
-    @GetMapping("/video-details")
-    public JsonResponse<Map<String, Object>>
-    getVideoDetails(@RequestParam Long videoId) {
-        Map<String, Object> result = videoService.getVideoDetails(videoId);
-        return new JsonResponse<>(result);
-    }
-
-    // 添加视频观看记录
-    @PostMapping("/video-views")
-    public JsonResponse<String> addVideoView(@RequestBody VideoView videoView,
-                                             HttpServletRequest request) {
-        Long userId;
-        try {
-            userId = userSupport.getCurrentUserId();
-            videoView.setUserId(userId);
-            videoViewService.addVideoView(videoView, request);
-            elasticSearchService.updateVideo(videoService.getById(videoView.getVideoId()));
-        } catch (Exception e) {
-            videoViewService.addVideoView(videoView, request);
-        }
-        return JsonResponse.success();
-    }
-
-    // 统计观看人数
-    @GetMapping("/video-view-counts")
-    public JsonResponse<Integer> getVideoViewCounts(@RequestParam Long videoId) {
-        Integer count = videoViewService.getVideoViewCounts(videoId);
-        return new JsonResponse<>(count);
-    }
-
-    // 个性化推荐
-    @GetMapping("/recommendation")
-    public JsonResponse<List<Video>> recommend() throws TasteException {
-        Long userId = userSupport.getCurrentUserId();
-        List<Video> list = videoService.recommend(userId);
-        return new JsonResponse<>(list);
-    }
+  // 个性化推荐
+  @GetMapping("/recommendation")
+  public JsonResponse<List<Video>> recommend() throws TasteException {
+    Long userId = userSupport.getCurrentUserId();
+    List<Video> list = videoService.recommend(userId);
+    return new JsonResponse<>(list);
+  }
 }
--- src/main/java/com/bilibili/dao/repository/AdPerformanceRepository.java
+++ src/main/java/com/bilibili/dao/repository/AdPerformanceRepository.java
@@ -3,6 +3,7 @@ package com.bilibili.dao.repository;
 import com.bilibili.dao.domain.TAdPerformance;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-public interface AdPerformanceRepository extends ElasticsearchRepository<TAdPerformance,Long> {
-    TAdPerformance findByAdId(Long adId);
+public interface AdPerformanceRepository
+    extends ElasticsearchRepository<TAdPerformance, Long> {
+  TAdPerformance findByAdId(Long adId);
 }
